d08cc630c1df97156789ecc7fbccbf33e730a85f
Joshua Challenger <jchallenger1@hotmail.com>
2023-08-31 23:52:56 -0400

    added view for url redirection

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import RedirectView
 
 from core.models import Url
 from core.forms import UrlForm
@@ -22,3 +23,25 @@ class HomeView(View):
         return render(
             request, self.template_name, {"short_url": obj.get_full_short_url()}
         )
+
+class HashUrlRedirectView(RedirectView):
+    # Redirects a hashedurl to the original url
+    # Responses as the original url or 410 if not found
+    # https://docs.djangoproject.com/en/4.2/ref/class-based-views/base/#redirectview
+    permanent = False
+    query_string = False
+    pattern_name = ""
+    
+    def get_redirect_url(self, *args, **kwargs):
+        
+        if "hashed_url" in kwargs:
+            matched_urls = Url.objects.filter(hashed_url=kwargs["hashed_url"])
+            if (len(matched_urls) == 1):
+                self.url = matched_urls[0].url
+        else:
+            # Note: Normally this would be put into some log file
+            print(f"Url [{self.request.get_full_path()}] was redirected but had no hashed_url associated.")
+        
+        # RedirectView handles the 410 or original url depending on if self.url is set to the url or None
+        return super().get_redirect_url(*args, **kwargs)
+        
